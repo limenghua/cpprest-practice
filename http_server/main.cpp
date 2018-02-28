ec16ef27ad800ee95015a5b446d22699ccaa9f52
李孟华 <576566385@qq.com>
2018-02-28 21:50:38 +0800

    simplify the code of main.cpp

--- http_server/main.cpp
+++ http_server/main.cpp
@@ -2,26 +2,15 @@
 // Created by limenghua on 18-2-28.
 //
 
-#include <boost/asio.hpp>
-
 #include <iostream>
-#include <pplx/pplxtasks.h>
 #include <cpprest/http_listener.h>
-#include <thread>
 
-using namespace boost::asio;
-using boost::asio::ip::tcp;
 using namespace web;
 using namespace http;
 using namespace http::experimental::listener;
 
-using namespace pplx;
-
-
 int main()
 {
-    std::cout<<"hello"<<std::endl;
-
     http_listener listener("http://127.0.0.1:3000");
 
     listener.support([](http_request message){
@@ -36,7 +25,6 @@ int main()
     std::cin.get();
 
     listener.close().get();
-
 
     return 0;
 }
